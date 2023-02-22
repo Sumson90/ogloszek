caeafca683c5f340359ba954ee270ff9d07e4efe
Sumson90 <Sumkowski.Tomasz@gmail.com>
2023-02-22 15:47:14 +0100

    Six commit

--- build-and-restart.sh
+++ build-and-restart.sh
@@ -1,4 +1,4 @@
-docker build -t ogloszek .
+docker build . -t ogloszek
 docker stop ogloszek || true
 docker rm ogloszek || true
-docker run -d -p 8080:8080 --name ogloszek ogloszek
+docker run -d -p 8080:8080 --name=ogloszek -e SPRING_PROFILES_ACTIVE=prod --network ogloszek-network --restart unless-stopped ogloszek
